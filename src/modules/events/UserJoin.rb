7be974a6e87a08cfe0407320d54b246cd8fd5186
Adam <aci747@engineer.com>
2018-01-10 14:30:40 +0000

    Create welcome event

---
+++ src/modules/events/UserJoin.rb
@@ -0,0 +1,11 @@
+module Bot::DiscordEvents
+  # Every time a user joins the server, send one of the set welcome messages.  
+  module UserJoin
+    extend Discordrb::EventContainer
+    user_join do |event|
+      welcome = ["Leave your weapons at the door.", "We just ran out of coffee.",
+      "You'll have to be better than that to not be spotted.", "Trust the cloak."]
+      "{#event.user.mention} has joined. {#welcome[rand(welcome.length -1)]}"
+    end
+  end
+end
