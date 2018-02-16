ff5d7062b3e3292d3e4c0d9e7f813a425fdb34fe
Adam <aci747@engineer.com>
2018-02-16 19:15:27 +0000

    Update userjoin.rb

--- src/modules/events/userjoin.rb
+++ src/modules/events/userjoin.rb
@@ -6,7 +6,7 @@ module Bot::DiscordEvents
       welcome = ['Leave your weapons at the door.', 'We just ran out of coffee.',
                  "You'll have to be better than that to not be spotted.", 'Trust the cloak.']
       event.server.default_channel.send("\u200B#{event.user.mention} 
-      has joined. #{welcome[rand(welcome.length - 1)])}"
+      has joined. #{welcome[rand(welcome.length - 1)]}")
     end
   end
 end
