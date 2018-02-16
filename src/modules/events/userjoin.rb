c5a01fd126db860edb2a683f40fbf43f2ed6de9d
Theotherunknown <aci747@engineer.com>
2018-02-16 14:04:05 -0500

    Also correct userjoin event

--- src/modules/events/userjoin.rb
+++ src/modules/events/userjoin.rb
@@ -2,7 +2,7 @@ module Bot::DiscordEvents
   # Every time a user joins the server, send one of the set welcome messages.
   module UserJoin
     extend Discordrb::EventContainer
-    member_join do |_event|
+    member_join do |event|
       welcome = ['Leave your weapons at the door.', 'We just ran out of coffee.',
                  "You'll have to be better than that to not be spotted.", 'Trust the cloak.']
       event.respond('\u200B' + event.user.mention + ' has joined. ' + welcome[rand(welcome.length - 1)])
