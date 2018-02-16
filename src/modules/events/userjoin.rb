391d4e1519d21c2d766872926fea01def1aee4e9
Theotherunknown <aci747@engineer.com>
2018-02-16 14:19:12 -0500

    Final testing for userevents

--- src/modules/events/userjoin.rb
+++ src/modules/events/userjoin.rb
@@ -5,8 +5,7 @@ module Bot::DiscordEvents
     member_join do |event|
       welcome = ['Leave your weapons at the door.', 'We just ran out of coffee.',
                  "You'll have to be better than that to not be spotted.", 'Trust the cloak.']
-      event.server.default_channel.send("\u200B#{event.user.mention} 
-      has joined. #{welcome[rand(welcome.length - 1)]}")
+      event.server.default_channel.send("\u200B#{event.user.mention} has joined. #{welcome[rand(welcome.length - 1)]}")
     end
   end
 end
