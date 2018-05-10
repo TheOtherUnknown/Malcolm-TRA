27c080f965ba0e37f3ac5e1cfd3f3c20e29a4432
Adam <aci747@engineer.com>
2018-05-09 21:36:09 -0400

    Correct ping module crash

--- src/modules/commands/ping.rb
+++ src/modules/commands/ping.rb
@@ -2,7 +2,7 @@ module Bot::DiscordCommands
   # Responds with "Pong!" and a time based on timestamps.
   module Ping
     extend Discordrb::Commands::CommandContainer
-    command :ping do |_event|
+    command :ping do |event|
       m = event.respond('Pong!')
       ping = (Time.now - event.timestamp)
       m.edit "Pong! Time taken: #{ping} seconds."
