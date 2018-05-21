a8288a3c2b312fe01c40025a02b0fbdd49e49010
Adam <aci747@engineer.com>
2018-05-21 12:30:57 -0400

    Add year condition, rate limit

--- src/modules/commands/verify.rb
+++ src/modules/commands/verify.rb
@@ -1,13 +1,15 @@
+# Create ratelimiting bucket
+BOT.bucket :wait60, delay: 60
 module Bot::DiscordCommands
   # Allow a user to verify themself after certian conditions are met
   module Verify
     extend Discordrb::Commands::CommandContainer
-    command :verify, description: 'Add yourself to the \"Verified\" role.', usage: 'verify' do |event|
+    command :verify, bucket: :wait60, description: 'Add yourself to the \"Verified\" role.', required_permissions: [:manage_roles], usage: 'verify' do |event|
       jointime = event.user.on(event.server).joined_at
-      if jointime.mon > Time.now.mon || Time.now.mday - jointime.mday > 3
+      if jointime.mon > Time.now.mon || Time.now.mday - jointime.mday > 3 || jointime.year < Time.now.year
         allroles = event.server.roles
         allroles.each do |a|
-          if configatron.verified.lower == a.name.lower
+          if configatron.verified.casecmp(a.name).zero?
             event.user.on(event.server).add_role(a)
             break
           end
