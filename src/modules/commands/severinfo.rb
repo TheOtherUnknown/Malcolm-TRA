dbb1826246a4b433df23cc7135f88041a9fabf7c
Adam <aci747@engineer.com>
2018-07-03 19:06:25 -0400

    Add cases for PMs, change leave event for bans (beta)

--- src/modules/commands/severinfo.rb
+++ src/modules/commands/severinfo.rb
@@ -3,12 +3,16 @@ module Bot::DiscordCommands
   module ServerInfo
     extend Discordrb::Commands::CommandContainer
     command(:serverinfo, description: 'Display this server\'s basic info.', usage: 'serverinfo') do |event|
-      event << "**#{event.server.name}**"
-      event << "Owner: #{event.server.owner.username}"
-      event << "Users: #{event.server.member_count}"
-      event << "Online: #{event.server.online_members.count}"
-      event << event.server.icon_url
-      event << "Region: #{event.server.region}"
+      if event.channel.type != 0
+        'Info cannot be retrieved for PMs!'
+      else
+        event << "**#{event.server.name}**"
+        event << "Owner: #{event.server.owner.username}"
+        event << "Users: #{event.server.member_count}"
+        event << "Online: #{event.server.online_members.count}"
+        event << event.server.icon_url
+        event << "Region: #{event.server.region}"
+      end
     end
   end
 end
--- src/modules/events/userleave.rb
+++ src/modules/events/userleave.rb
@@ -2,6 +2,10 @@ module Bot::DiscordEvents
   # Send a user leave message preceeded by a 0-width space.
   module UserPart
     extend Discordrb::EventContainer
+    user_ban do |event|
+      event.server.default_channel.send("\u200B#{event.user.name}\##{event.user.discrim} has been banned.")
+      return
+    end
     member_leave do |event|
       event.server.default_channel.send("\u200B#{event.user.name}\##{event.user.discrim} has left the server.")
     end
