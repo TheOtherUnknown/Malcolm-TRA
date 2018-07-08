9e510058870cef1b9982892feb6ade5d3f52b371
Adam <aci747@engineer.com>
2018-07-07 23:49:18 -0400

    Patch userjoin channel for discordrb master

--- src/modules/events/userjoin.rb
+++ src/modules/events/userjoin.rb
@@ -2,10 +2,11 @@ module Bot::DiscordEvents
   # Every time a user joins the server, send one of the set welcome messages.
   module UserJoin
     extend Discordrb::EventContainer
+    welcome = ['Leave your weapons at the door.', 'We just ran out of coffee.',
+               "You'll have to be better than that to not be spotted.", 'Trust the cloak.']
     member_join do |event|
-      welcome = ['Leave your weapons at the door.', 'We just ran out of coffee.',
-                 "You'll have to be better than that to not be spotted.", 'Trust the cloak.']
-      event.server.default_channel.send("\u200B#{event.user.mention} has joined. #{welcome[rand(welcome.length - 1)]}")
+      # TODO: find out how #text_channels is sorted. Why does this work?
+      event.server.text_channels[0].send("\u200B#{event.user.mention} has joined. #{welcome[rand(welcome.length - 1)]}")
     end
   end
 end
