deb58b98b9532ad7c027ad4c785c3b304c48b587
Adam <aci747@engineer.com>
2018-07-11 18:13:01 -0400

    Change game odds

--- src/modules/commands/rr.rb
+++ src/modules/commands/rr.rb
@@ -2,11 +2,23 @@ module Bot::DiscordCommands
   # A Russian roulette style game that might just get you kicked
   module RussianRoulette
     extend Discordrb::Commands::CommandContainer
-    command :rr, description: 'A simple game of daring.', usage: 'rr' do |event|
-      if rand(1..6) == 1
+    command :rr, description: 'A simple game of daring. Danger! Danger Will Robinson!', usage: 'rr' do |event|
+      if (defined? @gun).nil? || @gun.empty?
+        @gun = Array.new(5, false)
+        @gun[rand(5)] = true
+      end
+      chamber = rand(@gun.length - 1)
+      if @gun[chamber]
+        begin
         event.server.kick(event.user)
-        '*BANG!*'
+        @gun.clear
+        '**BANG!**'
+        rescue Discordrb::Errors::NoPermission
+          event.channel.send('Well that takes the fun out of things. No kick permissions.')
+          @gun.clear
+        end
       else
+        @gun.delete_at(chamber)
         'Click.'
       end
     end
