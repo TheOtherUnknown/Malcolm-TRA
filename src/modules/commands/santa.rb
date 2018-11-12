0e387a001f97022b10189b7addbf944d09430c6b
TheOtherUnknown <aci747@engineer.com>
2018-11-11 21:25:18 -0500

    Begin admin interface

--- src/modules/commands/santa.rb
+++ src/modules/commands/santa.rb
@@ -5,10 +5,10 @@ module Bot::DiscordCommands
   # Registration and admin for secret santa bot - Requires additional gems and config
   module SecretSanta
     extend Discordrb::Commands::CommandContainer
+    db = SQLite3::Database.new 'data/santa.sqlite3'
     command :santa do |event|
       break unless event.channel.type == 1
 
-      db = SQLite3::Database.new 'data/santa.sqlite3'
       event << 'Welcome to Secret Santa registration for the Ranger\'s Apprentice!'
       event << 'What would you like for Christmas? Think of a few Ranger\'s Apprentice themed gift ideas deliverable online.'
       sleep 3
@@ -37,18 +37,34 @@ module Bot::DiscordCommands
           info_event.respond('If everything looks okay, say `Submit` to finish or anything else to quit.')
           false
         when 4
-          #return 'Exiting...' unless info_event.message.content.casecmp?('submit')
+          return 'Exiting...' unless info_event.message.content.casecmp?('submit')
 
           begin
             db.execute("INSERT INTO users(username, uid, pref, other)
-              VALUES(?, ?, ?, ?)", userd[0], userd[1], userd[2], userd[3], userd[4])
-            puts userd
+              VALUES(?, ?, ?, ?)", userd[0], userd[1], userd[2], userd[3])
           rescue SQLite3::Exception => e
             info_event.respond("```#{e}```")
           else
             info_event.respond('Registraton succesful! You will recieve your match soon.')
           end
         end
+      end
+    end
+    command :raindeer do |event|
+      break unless event.user.id == configatron.owner
+
+      db.close
+      total_users = db.query('SELECT COUNT(*) FROM users')
+      if total_users.even?
+        # Run match
+        FileUtils.cp('data/santa.sqlite3', 'data/backup_santa.sqlite3')
+        FileUtils.chmod(0o444, 'data/backup_santa.sqlite3')
+        db.results_as_hash = true
+        db.execute('SELECT * FROM users') do |row|
+          # Send match messages
+        end
+      else
+        'What are the odds? The registration pool sure is! Please register yourself to make it even.'
       end
     end
   end
