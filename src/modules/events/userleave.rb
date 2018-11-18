dddd59b987e0710a052505f8edd16af8acf442f9
TheOtherUnknown <aci747@engineer.com>
2018-11-18 17:35:15 -0500

    Fix localjump error on userban

--- src/modules/events/userleave.rb
+++ src/modules/events/userleave.rb
@@ -4,7 +4,6 @@ module Bot::DiscordEvents
     extend Discordrb::EventContainer
     user_ban do |event|
       event.server.text_channels[0].send("\u200B#{event.user.name}\##{event.user.discrim} has been banned.")
-      return
     end
     member_leave do |event|
       event.server.text_channels[0].send_temporary_message("\u200B#{event.user.name}\##{event.user.discrim} has left the server.", 90)
