c3eaedc67a871195724a97438ba1ad46ae14046c
TheOtherUnknown <aci747@engineer.com>
2018-11-22 11:55:04 -0500

    Add config and giving info, use int for user ID

--- src/modules/commands/santa.rb
+++ src/modules/commands/santa.rb
@@ -6,18 +6,18 @@ module Bot::DiscordCommands
   module SecretSanta
     extend Discordrb::Commands::CommandContainer
     include Bot
-    db = SQLite3::Database.new 'data/santa.sqlite3'
+    db = SQLite3::Database.new 'data/santa.db'
     command :santa do |event|
-      break unless event.channel.type == 1
+      break unless event.channel.type == 1 && configatron.santa
 
       # Prepare the SQL insert statement for later, prevents SQL injection?
-      ins_sql = db.prepare("INSERT INTO users(username, uid, pref, other)
-                  VALUES(?, ?, ?, ?)")
+      ins_sql = db.prepare("INSERT INTO users(username, uid, pref, other, giving)
+                  VALUES(?, ?, ?, ?, ?)")
       event << 'Welcome to Secret Santa registration for the Ranger\'s Apprentice!'
       event << 'What would you like for Christmas? Think of a few Ranger\'s Apprentice themed gift ideas deliverable online.'
       sleep 3
       event << 'When you\'re ready, send one message detailing your prefrences. I\'ll wait until you\'re ready. Or, say Quit.'
-      userd = [(event.user.name + '#' + event.user.discrim).to_s, event.user.id.to_s]
+      userd = [(event.user.name + '#' + event.user.discrim).to_s, event.user.id]
       event.channel.await(:info) do |info_event|
         unless info_event.message.content.casecmp('quit').zero?
           case userd.length
@@ -28,6 +28,10 @@ module Bot::DiscordCommands
             false
           when 3
             userd.insert(-1, info_event.message.content)
+            info_event.respond('Got it. What would you like to *give* this year? (A specific oneshot, drawing, etc)')
+            false
+          when 4
+            userd.insert(-1, info_event.message.content)
             info_event.respond('Alright, here\'s what we have so far:')
             sleep 3
             info_event.respond("Username: **#{userd[0]}** (ID: #{userd[1]})")
@@ -37,17 +41,18 @@ module Bot::DiscordCommands
             info_event.respond('Other info:')
             info_event.respond(userd[3])
             sleep 3
-            info_event.respond('If everything looks okay, say `Submit` to finish or anything else to quit.')
+            info_event.respond('If everything looks okay, say `Submit` to finish or anything else to quit. Please remember that ghosting the event will result in a ban.')
             false
-          when 4
+          when 5
             if info_event.message.content.casecmp?('submit')
               begin
-                ins_sql.execute(userd[0], userd[1], userd[2], userd[3])
+                ins_sql.execute(userd[0], userd[1], userd[2], userd[3], userd[4])
               # This probably means the user is already registered, do update
               rescue SQLite3::ConstraintException
-                db.prepare('UPDATE users SET pref=?, other=? WHERE uid=?').execute(userd[2], userd[3], userd[1])
+                db.prepare('UPDATE users SET pref=?, other=?, giving=? WHERE uid=?').execute(userd[2], userd[3], userd[4], userd[1])
+                info_event.respond('You are already registered, attempting update')
               rescue SQLite3::Exception => e
-                info_event.respond("```#{e}```")
+                info_event.respond("Unable to write to DB! ```#{e}```")
               else
                 info_event.respond('Registraton succesful! You will recieve your match soon.')
               end
