d2d36b0b203d2cec48103a8fee49e8121cbe33d2
TheOtherUnknown <aci747@engineer.com>
2018-12-16 22:24:21 -0500

    Remove secret santa for the year :(

--- data/example.config.rb
+++ data/example.config.rb
@@ -4,7 +4,6 @@ configatron.client_id = 0 # The bot's client ID
 configatron.owner = 0 # The bot owner's user ID
 # Optional
 configatron.game = '' # Now playing content
-configatron.santa = false # Is secret santa registration open?
 configatron.verified = '' # Name of verified role
 configatron.welcome = ['Leave your weapons at the door.', 'We just ran out of coffee.',
                        "You'll have to be better than that to not be spotted.", 'Trust the cloak.', 'One riot. One Ranger.']
