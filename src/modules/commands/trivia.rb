1a645361e805e380665fff8a064dc4bd5ede2045
Adam I <15039983+TheOtherUnknown@users.noreply.github.com>
2019-03-01 12:36:48 -0500

    Cleanup loops and remove nonletters from answers (#3)
    * Begin transiton to case with timeout
    
    * Finish timeout with if instead
    
    * Cleanup unimportant chars in answers

--- src/modules/commands/trivia.rb
+++ src/modules/commands/trivia.rb
@@ -13,33 +13,40 @@ module Bot::DiscordCommands
         players.default = 0
         answer = event.message # Must be a message to start off with. Content only used in the loop
         event.respond('Starting trivia. The first to 5 points wins!')
-        until answer.content.casecmp?('stop')
+        loop do
           # Get a random question from the DB and store it in a Hash
           ques = db.query('SELECT question, answer FROM trivia WHERE id=?', 1 + rand(db.query('SELECT Count(*) FROM trivia').next[0])).next
           event.respond(ques['question']) # Ask the question
-          answer = event.channel.await!
-          if answer.content.casecmp?(ques['answer']) # You guessed right!
-            event.respond('You got it!')
-            sleep 3
-            players[answer.user.id] += 1
-          elsif answer.content.casecmp?('stop')
-            event.respond('Exiting...')
-          else # You guessed wrong!
-            event.respond('Nope! Moving on...')
-            sleep 3
+          answer = event.channel.await!(timeout: 60)
+          if answer # Was there an answer before the timeout?
+            cleanans = answer.content.gsub(/\W/, '').downcase # Clean up the answer by removing caps and non [a-z] [0-9] chars
+            if cleanans == ques['answer'] # There was an answer! You guessed right!
+              event.respond('You got it!')
+              sleep 3
+              players[answer.user.id] += 1
+            elsif cleanans == 'stop' # There was an answer! It was stop
+              event.respond('Exiting...')
+              break
+            else # There was an answer! It was wrong!
+              event.respond('Nope! Moving on...')
+              sleep 3
+            end
+          else # No answer
+            event.respond('Well, alright then. Exiting...')
+            break
           end
           next unless players.value?(5) # Rubocop insists on this instead of using if. Who knows why. Does someone have a score of 5?
           event.respond('And that\'s the game!')
           sleep 3
           event.respond("#{BOT.user(players.key(5)).name} wins!")
-          answer = 'stop' # Kill the loop
+          break # Kill the loop
         end
       # Add a new question
       elsif action == 'add' # && event.user.id == configatron.owner
         event.respond('Enter a new question: ')
         ques = event.user.await!.content
         event.respond('Enter the answer: ')
-        ans = event.user.await!.content
+        ans = event.user.await!.content.gsub(/\W/, '').downcase # Clean up the answer by removing caps and non [a-z] [0-9] chars
         event.respond('Commit to database? (y/n): ')
         if event.user.await!.content == 'y'
           begin
