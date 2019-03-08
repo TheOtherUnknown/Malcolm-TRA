c609e98c6d6379aacd96bd6741558a04366d0985
Adam Isaac <aci747@engineer.com>
2019-03-08 13:24:37 -0500

    Prevent abuse of the nuke command

--- src/modules/commands/nuke.rb
+++ src/modules/commands/nuke.rb
@@ -3,16 +3,12 @@ module Bot::DiscordCommands
   module NukeStuff
     extend Discordrb::Commands::CommandContainer
     command(:nuke, description: 'Delete a specified number of messages from the current channel.',
-                   usage: 'nuke number', min_args: 1, required_permissions: [:manage_messages]) do |event, ammount|
+                   usage: 'nuke number', min_args: 1, bucket: :wait90, required_permissions: [:manage_messages]) do |event, ammount|
       if event.bot.profile.on(event.server).permission? :manage_messages
         ammount = ammount.to_i
-        next "Can't delete less than 2 messages." if ammount < 2
-
-        while ammount > 100
-          event.channel.prune(100)
-          ammount -= 100
-        end
-        event.channel.prune(ammount) if ammount >= 2
+        event.respond("Can't delete less than 2 messages.") if ammount < 2
+        event.respond('Seriously, if you need to delete that many mesages, just delete the channel') if ammount > 99
+        event.channel.prune(ammount) if ammount >= 2 && ammount < 100
         nil
       end
     end
