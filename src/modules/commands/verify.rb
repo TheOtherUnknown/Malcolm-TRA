f6c3809ca9b7154f7ca9cfa2da1bd6d9edca1208
Adam Isaac <aci747@engineer.com>
2019-08-10 21:18:21 -0400

    Try to prevent null problems

--- src/modules/commands/verify.rb
+++ src/modules/commands/verify.rb
@@ -3,11 +3,10 @@ module Bot::DiscordCommands
   module Verify
     extend Discordrb::Commands::CommandContainer
     command :verify, bucket: :wait90, description: 'Add yourself to the \"Verified\" role.', usage: 'verify' do |event|
-      jointime = event.user.on(event.server).joined_at
       if event.user.permission?(:change_nickname)
         'You\'re already verified.'
       # User must be in the server at least 1 day
-      elsif (Time.now.utc - jointime).to_i >= 86_400
+      elsif (Time.now.utc - event.user.on(event.server).joined_at).to_i >= 86_400
         allroles = event.server.roles
         # Loop through all server roles to find role named in config.rb
         allroles.each do |a|
